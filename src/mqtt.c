fd06efbfd04fb6bf6f4536d7153fb7bda52792ad
Bassbabee <makcim510@mail.ru>
2021-01-25 23:13:30 +0300

    Added a few basic funcs to work with mqtt protocol

--- src/mqtt.c
+++ src/mqtt.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include "mqtt.h"
+#include "pack.h"
+
+static const int MAX_LEN_BYTES = 4;
 
 static size_t unpack_mqtt_connect(const unsigned char *raw, union mqtt_header *hdr, union mqtt_packet *pkt);
 static size_t unpack_mqtt_publish(const unsigned char *raw, union mqtt_header *hdr, union mqtt_packet *pkt);
@@ -12,3 +15,30 @@ static unsigned char *pack_mqtt_ack(const union mqtt_packet *pkt);
 static unsigned char *pack_mqtt_connack(const union mqtt_packet *pkt);
 static unsigned char *pack_mqtt_suback(const union mqtt_packet *pkt);
 static unsigned char *pack_mqtt_publish(const union mqtt_packet *pkt);
+
+extern int mqtt_encode_length(unsigned char *buf, size_t len) {
+    int bytes = 0;
+    do {
+        if (bytes + 1 > MAX_LEN_BYTES)
+            return bytes;
+        short d = len % 128;
+        len /= 128;
+        if (len > 0)
+            d |= 128;
+        buf[bytes++] = d;
+    } while (len > 0);
+    return bytes;
+}
+
+extern unsigned long long mqtt_decode_length(const unsigned char **buf){
+    char c;
+    int multiplier = 1;
+    unsigned long long value = 0LL;
+    do {
+        c = **buf;
+        value += (c % 127) * multiplier;
+        multiplier *= 128;
+        (*buf)++;
+    } while ((c & 128) != 0);
+    return value;
+}
--- src/pack.h
+++ src/pack.h
@@ -6,26 +6,26 @@
 
 /* BEGIN READ DATA */
 // bytes -> uint8_t
-uint8_t unpack_u8(const uint8_t **buf);
+extern uint8_t unpack_u8(const uint8_t **buf);
 // butes -> uint16_t
-uint16_t unpack_u16(const uint8_t **buf);
+extern uint16_t unpack_u16(const uint8_t **buf);
 // butes -> uint32_t
-uint32_t unpack_u32(const uint8_t **buf);
+extern uint32_t unpack_u32(const uint8_t **buf);
 // get length of bytes
-uint8_t *unpack_bytes(const uint8_t **buff, size_t len, uint8_t *str);
+extern uint8_t *unpack_bytes(const uint8_t **buf, size_t len, uint8_t *str);
 // get a string prefix by its length
-uint16_t unpack_string16(uint8_t **buf, uint8_t **dest);
+extern uint16_t unpack_string16(uint8_t **buf, uint8_t **dest);
 /* END READ DATA */
 
 /* BEGIN WRITE DATA */
 // append uint8_t into bytestring
-void pack_u8(uint8_t **buf, uint8_t val);
+extern void pack_u8(uint8_t **buf, uint8_t val);
 // append uint16_t into bytestring
-void pack_u16(uint8_t **buf, uint16_t val);
+extern void pack_u16(uint8_t **buf, uint16_t val);
 // append uint32_t into bytestring
-void pack_u32(uint8_t **buf, uint32_t val);
+extern void pack_u32(uint8_t **buf, uint32_t val);
 // append length bytes into bytestring
-void pack_bytes(uint8_t **buf, uint8_t *str);
+extern void pack_bytes(uint8_t **buf, uint8_t *str);
 /* END WRITE DATA */
 
 #endif
